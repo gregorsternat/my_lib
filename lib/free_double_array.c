2f51dd3001c094ee7eb300cac526d5ab205840a0
gregorsternat <gregor.sternat@epitech.eu>
2024-03-03 20:42:40 +0100

    feat: adding a function to free a double array

---
+++ lib/free_double_array.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** my_lib
+** File description:
+** free_double_array.c
+*/
+
+#include <stddef.h>
+#include <stdlib.h>
+
+void free_double_array(void **array)
+{
+    for (size_t i = 0; array[i] != NULL; i++) {
+        free(array[i]);
+    }
+    free(array);
+}
