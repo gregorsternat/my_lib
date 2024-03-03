38b2a66507d1938061d1c4110004d5ca5ee3486d
gregorsternat <gregor.sternat@epitech.eu>
2024-03-03 20:48:00 +0100

    feat: adding a function to duplicate a string

---
+++ lib/my_strdup.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** my_lib
+** File description:
+** my_strdup.c
+*/
+
+#include <stddef.h>
+#include <stdlib.h>
+#include "my.h"
+
+char *my_strdup(const char *src)
+{
+    size_t src_len = 0;
+    char *dest = NULL;
+
+    if (src == NULL)
+        return NULL;
+    src_len = my_strlen(src);
+    dest = malloc(sizeof(char) * (src_len + 1));
+    if (dest == NULL)
+        return NULL;
+    for (size_t i = 0; i != src_len; i++) {
+        dest[i] = src[i];
+    }
+    dest[src_len] = '\0';
+    return dest;
+}
