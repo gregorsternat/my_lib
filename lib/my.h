cd6e1a7abbd67cdc4f41b7e1af94157b4a39f4bc
gregorsternat <gregor.sternat@epitech.eu>
2024-03-03 20:37:09 +0100

    feat: adding my header

---
+++ lib/my.h
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2024
+** my_lib
+** File description:
+** my.h
+*/
+
+#ifndef MY_H
+    #define MY_H
+
+    #include <stddef.h>
+    #include <stdbool.h>
+
+size_t my_strlen(const char *str);
+char *file_to_str(const char *path);
+void free_double_array(void **array);
+int put_error(const char *msg);
+bool my_str_is_alpha(const char *str);
+bool my_str_is_lower(const char *str);
+bool my_str_is_upper(const char *str);
+bool my_str_is_num(const char *str);
+char *my_strdup(const char *src);
+
+#endif
