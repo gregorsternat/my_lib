2143f7b27352124bc70b4caf0c3aef523d766de9
gregorsternat <gregor.sternat@epitech.eu>
2024-03-03 20:42:24 +0100

    feat: adding function function for converting the contents of a file into a buffer

---
+++ lib/file_to_str.c
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2024
+** my_lib
+** File description:
+** file_to_str.c
+*/
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+char *file_to_str(const char *path)
+{
+    int fd = open(path, O_RDONLY);
+    struct stat file_stat;
+    char *buff = NULL;
+
+    if (fd == -1)
+        return NULL;
+    if (stat(path, &file_stat) == -1)
+        return NULL;
+    buff = malloc(sizeof(char) * (file_stat.st_size));
+    if (buff == NULL)
+        return NULL;
+    if (read(fd, buff, file_stat.st_size) == -1)
+        return NULL;
+    buff[file_stat.st_size] = '\0';
+    if (close(fd) == -1)
+        return NULL;
+    return buff;
+}
