0bf16a3af63539c0d9baaed2b4dc4534e538ee38
gregorsternat <gregor.sternat@epitech.eu>
2024-06-08 12:05:33 +0200

    style: moove str is function to a specific folder

---
+++ lib/str_is/my_str_is_num.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** my_lib
+** File description:
+** Checks whether a string contains only numbers
+*/
+
+#include <stdbool.h>
+#include <stddef.h>
+
+bool my_str_is_num(const char *str)
+{
+    for (size_t i = 0; str[i] != '\0'; i++) {
+        if (str[i] < '0' || str[i] > '9') {
+            return false;
+        }
+    }
+    return true;
+}
