27bd61a6a7c77dfb4f7c3de62021b3184e5846df
gregorsternat <gregor.sternat@epitech.eu>
2024-06-08 12:06:41 +0200

    style: move file to folder

--- lib/my.h
+++ lib/my.h
@@ -12,13 +12,7 @@
     #include <stdbool.h>
     #include "linked_list.h"
 
-size_t my_strlen(const char *str);
-
-char *file_to_str(const char *path);
-
-void free_double_array(void **array);
-
-int put_error(const char *msg);
+// str is
 
 bool my_str_is_alpha(const char *str);
 
@@ -28,13 +22,29 @@ bool my_str_is_upper(const char *str);
 
 bool my_str_is_num(const char *str);
 
-char *my_strdup(const char *src);
-
-size_t array_len(const void *const *arr);
+// linked list
 
 void push_to_list(linked_list_t **begin, void *data);
 
 void add_to_list(linked_list_t **begin, void *data);
+
+// array management
+
+void free_double_array(void **array);
+
+size_t array_len(const void *const *arr);
+
+// others
+
+size_t my_strlen(const char *str);
+
+char *file_to_str(const char *path);
+
+
+int put_error(const char *msg);
+
+char *my_strdup(const char *src);
+
 
 int my_put_str(const char *str);
 
